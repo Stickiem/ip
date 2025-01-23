8d08b80b8ee93e0c9556d4a542a8937acbeb67ab
Stickiem <e1138301@u.nus.edu>
2025-01-23 23:52:15 +0800

    Update Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,23 @@
 public class Task {
     private String name;
+    private boolean status;
 
-    public void setName(String name) {
+    public Task(String name) {
         this.name = name;
+        this.status = false;
     }
     public String getName() {
         return this.name;
+    }
+    public void unmarkStatus() {
+        this.status = false;
+    }
+
+    public void markStatus() {
+        this.status = true;
+    }
+    public String getDetails() {
+        String marking = status ? "X" : " ";
+        return "[" + marking + "] " + name;
     }
 }
