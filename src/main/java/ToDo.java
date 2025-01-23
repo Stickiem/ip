b6412d829ae24ae3a2c17a3d808bf8602364fa3d
Stickiem <e1138301@u.nus.edu>
2025-01-24 07:52:26 +0800

    Add ToDo.java

---
+++ src/main/java/ToDo.java
@@ -0,0 +1,12 @@
+public class ToDo extends Task {
+
+    public ToDo(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getDetails() {
+        String marking = status ? "X" : " ";
+        return "[T][" + marking + "] " + name;
+    }
+}
