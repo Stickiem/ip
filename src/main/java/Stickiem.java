56b96145c0677419aba4e84719ed7b042f6a31af
Stickiem <e1138301@u.nus.edu>
2025-01-23 23:22:22 +0800

    Add List function

--- src/main/java/Stickiem.java
+++ src/main/java/Stickiem.java
@@ -1,4 +1,6 @@
-import java.io.PrintWriter;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Stickiem {
@@ -8,12 +10,27 @@ public class Stickiem {
         output += "\n____________________________________________________________";
         System.out.println(output);
         Scanner input = new Scanner(System.in);
+        List<Task> storage = new ArrayList<Task>();
         while(true) {
+            output = "";
             String userInput = input.nextLine();
             if(userInput.equals("bye")) {
                 break;
+
+            } else if(userInput.equals("list")) {
+                int len = storage.size();
+
+                for(int i = 0; i < len; i++) {
+                    int index = i + 1;
+                    output += "\n" + index + ". " + storage.get(i).getName() ;
+
+                }
             } else {
-                output = userInput;
+
+                Task newItem = new Task();
+                newItem.setName(userInput);
+                storage.add(newItem);
+                output = "added: " + userInput;
             }
             output += "\n____________________________________________________________";
             System.out.println(output);
