3a1a77ec2964e6551f3b3681574e9f5356f1b4b1
Stickiem <e1138301@u.nus.edu>
2025-01-23 20:59:56 +0800

    Add Stickiem.java

---
+++ src/main/java/Stickiem.java
@@ -0,0 +1,13 @@
+import java.io.PrintWriter;
+
+public class Stickiem {
+    public static void main(String[] args) {
+        String output = "____________________________________________________________";
+        output += "\nHello! I'm Stickiem!\nWhat can I do for you?";
+        output += "\n____________________________________________________________";
+        output += "\nBye. Hope to see you again soon!";
+        output += "\n____________________________________________________________";
+        System.out.println(output);
+
+    }
+}
