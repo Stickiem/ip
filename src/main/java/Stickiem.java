be508102a25fff48e655c4af1803b7313c94da86
Stickiem <e1138301@u.nus.edu>
2025-01-23 23:52:53 +0800

    Add Mark as Done function

--- src/main/java/Stickiem.java
+++ src/main/java/Stickiem.java
@@ -17,18 +17,33 @@ public class Stickiem {
             if(userInput.equals("bye")) {
                 break;
 
-            } else if(userInput.equals("list")) {
+            } else if(userInput.contains("mark")) {
+                String[] details = userInput.split(" ");
+                Task currentTask = storage.get(Integer.parseInt(details[1]) - 1);
+                if(userInput.contains("unmark")) {
+                    currentTask.unmarkStatus();
+                    output += "OK, I've marked this task as not done yet:";
+
+                } else {
+                    currentTask.markStatus();
+                    output += "Nice! I've marked this task as done:";
+                }
+                output += "\n" + currentTask.getDetails();
+
+
+
+            }  else if(userInput.equals("list")) {
                 int len = storage.size();
 
                 for(int i = 0; i < len; i++) {
                     int index = i + 1;
-                    output += "\n" + index + ". " + storage.get(i).getName() ;
+                    Task currentTask = storage.get(i);
+                    output += "\n" + index + "."  + currentTask.getDetails();
 
                 }
             } else {
 
-                Task newItem = new Task();
-                newItem.setName(userInput);
+                Task newItem = new Task(userInput);
                 storage.add(newItem);
                 output = "added: " + userInput;
             }
