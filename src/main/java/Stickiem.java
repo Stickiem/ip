1e967a4c7f995211eeff633e92bc63c10edfcbb7
Stickiem <e1138301@u.nus.edu>
2025-01-23 21:15:29 +0800

    Add Echo function

--- src/main/java/Stickiem.java
+++ src/main/java/Stickiem.java
@@ -1,11 +1,24 @@
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Stickiem {
     public static void main(String[] args) {
         String output = "____________________________________________________________";
         output += "\nHello! I'm Stickiem!\nWhat can I do for you?";
         output += "\n____________________________________________________________";
-        output += "\nBye. Hope to see you again soon!";
+        System.out.println(output);
+        Scanner input = new Scanner(System.in);
+        while(true) {
+            String userInput = input.nextLine();
+            if(userInput.equals("bye")) {
+                break;
+            } else {
+                output = userInput;
+            }
+            output += "\n____________________________________________________________";
+            System.out.println(output);
+        }
+        output = "Bye. Hope to see you again soon!";
         output += "\n____________________________________________________________";
         System.out.println(output);
 
